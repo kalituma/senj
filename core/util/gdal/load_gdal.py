161f726390c4ee99c861fe81324698977213225e
kalituma <kalituma@gmail.com>
2024-11-25 14:58:20 +0900

    commit before making new branch for refactoring

--- core/util/gdal/load_gdal.py
+++
@@ -1,9 +0,0 @@
-import os
-from osgeo.gdal import Dataset
-from core.util.gdal import read_tif
-
-def load_raster_gdal(paths:list[str]) -> list[Dataset]:
-
-    for path in paths:
-        assert os.path.splitext(path)[1] == os.path.splitext(paths[0])[1], f'All input files should have the same extension'
-    return [read_tif(path) for path in paths]
